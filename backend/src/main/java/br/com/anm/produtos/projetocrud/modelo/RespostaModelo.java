53ee82bbc044b4a18eca0f40133e3c269cbd2b5b
rayssamorei <rayssa32alvess@gmail.com>
2025-03-10 08:02:48 -0300

    Criação da classe resposta modelo

---
+++ backend/src/main/java/br/com/anm/produtos/projetocrud/modelo/RespostaModelo.java
@@ -0,0 +1,12 @@
+package br.com.anm.produtos.projetocrud.modelo;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Setter
+@Getter
+public class RespostaModelo {
+    private Spring resposta;
+}
