97318e2a4d91549c36b165788442a800f0325629
rayssamorei <rayssa32alvess@gmail.com>
2025-03-10 07:51:57 -0300

    Criação método listar() do serviço

--- backend/src/main/java/br/com/anm/produtos/projetocrud/repositorio/ProdutoRepositorio.java
+++ backend/src/main/java/br/com/anm/produtos/projetocrud/repositorio/ProdutoRepositorio.java
@@ -1,7 +1,9 @@
 package br.com.anm.produtos.projetocrud.repositorio;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import br.com.anm.produtos.projetocrud.modelo.ProdutoModelo;
 
+@Repository
 public interface ProdutoRepositorio extends CrudRepository<ProdutoModelo, Long> {
     
 }
--- backend/src/main/java/br/com/anm/produtos/projetocrud/servico/ProdutoServico.java
+++ backend/src/main/java/br/com/anm/produtos/projetocrud/servico/ProdutoServico.java
@@ -1,5 +1,16 @@
 package br.com.anm.produtos.projetocrud.servico;
 
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.anotation.Autowired;
+import br.com.anm.produtos.projetocrud.repositorio.ProdutoRepositorio;
+import br.com.anm.produtos.projetocrud.modelo.ProdutoModelo;
+
+@Service
 public class ProdutoServico {
-    
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
 }
