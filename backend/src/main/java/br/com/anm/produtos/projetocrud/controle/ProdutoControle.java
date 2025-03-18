c06476b93c2fa0932a74ba0b9a259293a6fc6cdd
rayssamorei <rayssa32alvess@gmail.com>
2025-03-18 08:43:49 -0300

    Criação do método /alterar

--- backend/src/main/java/br/com/anm/produtos/projetocrud/controle/ProdutoControle.java
+++ backend/src/main/java/br/com/anm/produtos/projetocrud/controle/ProdutoControle.java
@@ -1,7 +1,11 @@
 package br.com.anm.produtos.projetocrud.controle;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import br.com.anm.produtos.projetocrud.servico.ProdutoServico;
 import br.com.anm.produtos.projetocrud.modelo.ProdutoModelo;
 
@@ -11,13 +15,22 @@ public class ProdutoControle {
     @Autowired
     private ProdutoServico ps;
 
-    @GetMapping("/listar")
-    public Iterable<ProdutoModelo> listar(){
+    @GetMapping("/listar") public Iterable<ProdutoModelo> listar() {
         return ps.listar();
     }
 
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+    
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+
     @GetMapping("/")
-    public String rota(){
+    public String rota() {
         return "A API está funcionando!";
     }
 }
--- backend/src/main/java/br/com/anm/produtos/projetocrud/servico/ProdutoServico.java
+++ backend/src/main/java/br/com/anm/produtos/projetocrud/servico/ProdutoServico.java
@@ -2,15 +2,40 @@ package br.com.anm.produtos.projetocrud.servico;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import br.com.anm.produtos.projetocrud.repositorio.ProdutoRepositorio;
 import br.com.anm.produtos.projetocrud.modelo.ProdutoModelo;
+import br.com.anm.produtos.projetocrud.modelo.RespostaModelo;
 
 @Service
 public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
+    @Autowired
+    private RespostaModelo rm;
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
     }
+
+    public ResponseEntity <?> cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setMensagem("A marca do produto é obrigatório!");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+                
+            }
+        }
+    }
+
+
 }
